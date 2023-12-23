7d2ceff6876f59866e7c867188d4abc079060017
ry05k2ulv <ryosuke.10117@gmail.com>
2023-12-23 18:03:32 +0900

    feat: Output image.png

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,29 @@
-fun main(args: Array<String>) {
-	println("Hello World!")
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+import kotlin.system.exitProcess
 
-	// Try adding program arguments via Run/Debug configuration.
-	// Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-	println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+	if (args.size != 1) {
+		System.err.println("Usage: MainKt <output file>")
+		exitProcess(1)
+	}
+	val outputFile = args[0]
+
+	val (imageWidth, imageHeight) = 256 to 256
+	val image = BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB)
+
+	repeat(imageHeight) { y ->
+		System.err.print("\rScanlines remaining: %03d".format(imageHeight - y))
+		repeat(imageWidth) { x ->
+			val color = (x shl 8) or (y shl 0)
+			image.setRGB(x, y, color)
+		}
+	}
+	
+	println("\rDone.                        ")
+	val file = File(outputFile)
+	ImageIO.write(image, "png", file)
+
+
 }
